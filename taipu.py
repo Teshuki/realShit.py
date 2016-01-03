d11859a11c9d8ccf9a3153b9c1e5585d70bed448
Teshuki <johnnava_2@hotmail.com>
2016-01-02 23:44:54 -0430

    nm1

---
+++ taipu.py
@@ -0,0 +1,14 @@
+#Write a program that asks the user for a Login Name and password. Then when they type "lock", they need to type in their name and password to unlock the program.
+
+while "bitch"=="bitch":
+    name=input("Type your username.\n")
+    if name!="John":
+        continue
+    password=input("Type the password.\n")
+    if password!="yes":
+        continue
+    print("'sup, John. Type 'lock' in case you don't want niggers to acces your valuable information. Type anything else to exit")
+    taipu=input("")
+    if taipu!="lock":
+        break
+print("Goodbye")
